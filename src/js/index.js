151f300fc9eddcf40f29a10c13eab938d0c2ae81
Steven Bayer <sbayer55@gmail.com>
2016-07-26 14:44:27 -0500

    Added props and state to App
    - Able to append items to a list in state

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,61 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-const App = (props) => (
-  <div id="app">
-    {'Hello React, Gulp, Less, es6, and JSX!'}
-  </div>
-)
+let list = []
 
-render(<App />, document.getElementById('react-mounting-point'))
+for (let n = 0; n < 10; n++) {
+  list.push('N = ' + n)
+}
+console.log('List = ', list)
+
+
+class BoldText extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return <span style={{fontWeight: '900'}}>{this.props.text}</span>
+  }
+}
+
+
+class Item extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return <div>- {this.props.children}</div>
+  }
+}
+// <span style="display: block">
+
+
+class App extends Component {
+
+  // How to make an App
+  constructor(props) {
+    super(props)
+
+    this.state = { mutableList: props.immutableList }
+    this.append = this.append.bind(this)
+  }
+
+  append() {
+    let newMutableList = this.state.mutableList
+    newMutableList.push('Appended string!')
+    this.setState({mutableList: newMutableList})
+  }
+
+  // How to draw this Component (App)
+  render() {
+    return <div>
+      <BoldText text={this.props.name} /><br />
+      <button onClick={this.append}>Add Item</button>
+      <br />
+      I have {this.state.mutableList.length} items!
+      {this.state.mutableList.map(string => (<Item>{string}</Item>))}
+    </div>
+  }
+}
+
+render(<App immutableList={list} name="React App" />, document.getElementById('react-mounting-point'))
